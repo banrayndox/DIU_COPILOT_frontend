dee8fa9de87257576d302e3eb5eb55b6ee3ad211
banrayndox <banrayndox@gmail.com>
2026-06-07 18:04:53 +0600

    commiting dockerfile

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: true, // allows access outside container
+    port: 5173,
+    strictPort: true,
+  },
   
 })
